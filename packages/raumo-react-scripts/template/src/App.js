41d9abb1320f1ec6ca7158e4a4d8fd93f949aa9e
Liquid2Sky <liquid2sky@gmail.com>
2018-06-14 16:34:04 +0200

    linter script + linteded

--- packages/raumo-react-scripts/template/src/App.js
+++ packages/raumo-react-scripts/template/src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './App.scss';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import './App.scss'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>
             Welcome to raumos variation of react-create-app template{' '}
           </h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Store: </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- packages/raumo-react-scripts/template/src/index.js
+++ packages/raumo-react-scripts/template/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import configureStore from './configureStore.dev';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { Provider } from 'react-redux'
+import configureStore from './configureStore.dev'
 // import { createStore } from 'redux'
-import fooApp from './reducers';
-import registerServiceWorker from './registerServiceWorker';
+// import fooApp from './reducers'
+// import registerServiceWorker from './registerServiceWorker'
 // Don't do this! You’re bringing DevTools into the production bundle.
-import DevTools from './DevTools';
-const store = configureStore();
+import DevTools from './DevTools'
+const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,5 +19,5 @@ ReactDOM.render(
     </div>
   </Provider>,
   document.getElementById('root')
-);
-registerServiceWorker();
+)
+// registerServiceWorker()
