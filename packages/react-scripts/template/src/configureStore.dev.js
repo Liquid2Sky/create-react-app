3a3364722d05519aa0bc843cafa4fc5bc13a8e22
Philipp Bohnenstengel <phibo23@users.noreply.github.com>
2018-08-13 15:05:22 +0200

    eslint rules

--- packages/react-scripts/template/src/configureStore.dev.js
+++ packages/react-scripts/template/src/configureStore.dev.js
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import { persistState } from 'redux-devtools';
-import rootReducer from './reducers';
-import DevTools from './DevTools';
+import { createStore, /* applyMiddleware, */ compose } from 'redux'
+import { persistState } from 'redux-devtools'
+import rootReducer from './reducers'
+import DevTools from './DevTools'
 
 const enhancer = compose(
   // Middleware you want to use in development:
@@ -10,19 +10,19 @@ const enhancer = compose(
   DevTools.instrument(),
   // Optional. Lets you write ?debug_session=<key> in address bar to persist debug sessions
   persistState(getDebugSessionKey())
-);
+)
 
-function getDebugSessionKey() {
+function getDebugSessionKey () {
   // You can write custom logic here!
   // By default we try to read the key from ?debug_session=<key> in the address bar
-  const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/);
-  return matches && matches.length > 0 ? matches[1] : null;
+  const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/)
+  return matches && matches.length > 0 ? matches[1] : null
 }
 
-export default function configureStore(initialState) {
+export default function configureStore (initialState) {
   // Note: only Redux >= 3.1.0 supports passing enhancer as third argument.
   // See https://github.com/rackt/redux/releases/tag/v3.1.0
-  const store = createStore(rootReducer, initialState, enhancer);
+  const store = createStore(rootReducer, initialState, enhancer)
 
   // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
   // if (module.hot) {
@@ -31,5 +31,5 @@ export default function configureStore(initialState) {
   //   );
   // }
 
-  return store;
+  return store
 }
