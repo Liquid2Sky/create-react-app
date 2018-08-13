3b496b40c489708784f76a50783393b5761b80bb
Liquid2Sky <liquid2sky@gmail.com>
2018-08-13 11:54:35 +0200

    styleguideconfig dependen on enviroment

--- packages/react-scripts/template/styleguide.config.js
+++ packages/react-scripts/template/styleguide.config.js
@@ -1,4 +1,12 @@
-module.exports = {
-  webpackConfig: require('raumo-react-scripts/config/webpack.config.prod.js'),
-  ignore: ['**/components/index.js']
+if(process.env.NODE_ENV === 'production') {
+  module.exports = {
+    webpackConfig: require('./node_modules/@raumobil/react-scripts/config/webpack.config.prod.js'),
+    ignore: ['**/components/index.js']
+  }
+} else {
+  module.exports = {
+    webpackConfig: require('./node_modules/@raumobil/react-scripts/config/webpack.config.dev.js'),
+    ignore: ['**/components/index.js']
+  }
 }
+
